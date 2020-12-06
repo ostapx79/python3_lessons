bb9173603343e8d1f7f0d2447ab7fc5c56b837b4
Constantin Galus <ostapx79@gmail.com>
2020-12-07 02:07:39 +0300

    переменные и типы данных

---
+++ lesson1/lesson1_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#: python3_lessons/lesson1/lesson1_2.py
+
+# сылка на ячейку в память П/К (переменая)
+# и типы данных
+
+numb = 32 # целое число
+strings = 'hello' # срока
+strings_s = "world" # тоже строка
+float_f = .4 # веществинные числа
+lists = [numb, strings, strings_s, float_f] # список
+tuple_t = (3, ) # кортеж из одного елемента
+dict_d = {'tuple': tuple_t, 'list': lists} # дикционар из двух елементов
+
+print(numb)
+print(strings)
+print(strings_s)
+print(float_f)
+print(lists)
+print(tuple_t)
+print(dict_d)
