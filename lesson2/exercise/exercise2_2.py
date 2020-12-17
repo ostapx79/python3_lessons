e4c1ebc8cbc93b3e6864ce553f4a10d12586b404
Constantin Galus <ostapx79@gmail.com>
2020-12-18 02:10:11 +0300

    второе упражнение по второму уроку на Python 3

---
+++ lesson2/exercise/exercise2_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#: py3_less/python3_lesson/lesson2/exercise/exercise2_2.py
+# 2.2 Простые сообщения: сохраните текстовое сообщение в переменой и выведите
+# это сообщение. Затем замените значение переменой другим сообщением и выведите
+# новое сообщение.
+
+variables = "Привет, python 3 мирно но ли!"
+print(variables)
+
+variables = 'Hello, python 3 super!'
+print(variables)
+
