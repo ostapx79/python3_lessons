48f71ed96dac75e57e24e56de1f04bcbaff4f366
Constantin Galus <ostapx79@gmail.com>
2020-12-18 02:14:32 +0300

    написаны комментарий к файлу первое упражнение по второму уроку на Python 3

--- lesson2/exercise/exercise2_1.py
+++ lesson2/exercise/exercise2_1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 #: py3_less/python3_lesson/lesson2/exercise/exercise2_1.py
+# 2.1 Простое сообщение: сохраните текстовое сообщение в переменной и выведите
+# его на экран.
 
 variable = "Привет Python на русском мир!"
 print(variable)
