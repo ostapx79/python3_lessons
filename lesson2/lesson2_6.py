aa935a2376e1fc06db0b61a0779fea8462fb9a51
Constantin Galus <ostapx79@gmail.com>
2020-12-20 01:43:35 +0300

    строки pyton3, и регистра символов в сроках

---
+++ lesson2/lesson2_6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#: python3_lesson/lesson2/lesson2_6.py
+# Изменение регистра символов с строках
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
