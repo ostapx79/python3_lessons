2be9eec001e95113f2618bb812e988c62d75bc52
Constantin Galus <ostapx79@gmail.com>
2020-12-20 01:14:01 +0300

    сроки в python3

---
+++ lesson2/lesson2_5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#: python3_lesson/lesson2/lesson2_5.py
+
+# Строки в python 3
+
+messages = 'This is a string.'
+message = "This is slso a string."
+
+message_one = 'I told my friend, "Python is my favorite language!'
+message_two = "The language 'Python' is named after Nonty Python, not the snake."
+message_three = "One of Python's strengths is its diverse and supportive community"
+
+print(messages)
+print(message)
+print(message_one)
+print(message_two)
+print(message_three)
